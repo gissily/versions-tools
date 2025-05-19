1a4b7e97a4d60e9c1b0c113363103f3ff633f846
gissily <gissily@outlook.com>
2025-05-19 13:15:31 +0800

    fix format

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ fi
 RELEASE=$(curl -s https://api.github.com/repos/gissily/versions-tools/releases/latest  | grep tag_name | cut -d '"' -f 4)
 
 if [[ "${RELEASE}" == "" ]]; then
-	echo "fetching latest version failure"
-	exit 1
+  echo "fetching latest version failure"
+  exit 1
 fi
 
 DOWNLOAD_URL="https://github.com/gissily/versions-tools/releases/download/${RELEASE}/versions-${BIN_SUFFIX}"
